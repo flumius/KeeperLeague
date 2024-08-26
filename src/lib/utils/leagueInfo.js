efd87cd742c0547489b27a94a361cd8f59c1a94d
flumius <98245594+flumius@users.noreply.github.com>
2024-08-25 22:04:10 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = 1048386147694567424; // your league ID
+export const leagueID = "1048386147694567424"; // your league ID
 export const leagueName = "Carolina Keeper"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
